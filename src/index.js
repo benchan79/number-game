086d1926eecb420136c265c60b56758d8e603aee
Ben Chan <benchan79@gmail.com>
2023-01-06 21:46:50 +0800

    Add index.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
+import './index.css';
 import { AuthContextProvider } from './context/AuthContextProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
